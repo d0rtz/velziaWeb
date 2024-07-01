5c09b968c19b7c03b1732d0a52e55329b9245c04
Diego Ortiz <diegoortizruescas@gmail.com>
2024-07-01 16:53:57 +0200

    fix

--- js/piso.js
+++ js/piso.js
@@ -167,7 +167,7 @@ function relatedProjects(pisos) {
       relatedProjectsArray.push(piso);
     }
   });
-  for (let i = 0; i < 9 || relatedProjectsArray.length; i++) {
+  for (let i = 0; i < 9 || i < relatedProjectsArray.length; i++) {
     relatedProjectsHtml += `
       <li class="splide__slide">
         <a href="piso.html?id=${
