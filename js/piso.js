179ccc6ddb093dab3139124b184f880a2b05b8b9
Diego Ortiz <diegoortizruescas@gmail.com>
2024-07-01 11:53:16 +0200

    fix

--- js/piso.js
+++ js/piso.js
@@ -223,7 +223,7 @@ function renderizarGaleria(data) {
 
 function initializeSplide() {
   var main = new Splide( '#image-slider', {
-    type       : 'loop',
+    type       : 'slide',
     perPage: 1,
     perMove: 1,
     height: '600px',
